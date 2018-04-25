6afac558e0889f96bc5e12e38715982c130dba01
TanyaKolp <lancerchg@gmail.com>
2018-04-25 10:53:16 +0300

    SCC_1:  * tests

--- src/test/java/ru/tanya/controller/ApplicationControllerTest.java
+++ src/test/java/ru/tanya/controller/ApplicationControllerTest.java
@@ -5,13 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -30,10 +30,10 @@ public class ApplicationControllerTest {
 
     @Test
     public void requestForNonexistentContact() throws Exception {
-        mockMvc.perform(get("/last_application/100"))
+        mockMvc.perform(get("/last_application/100")
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andDo(print())
-                .andExpect(status().isOk())
-        .andExpect(content().string("No contact #100"));
+                .andExpect(status().isNotFound());
     }
 
 }
